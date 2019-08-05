20eba6fa285a51892be03227cc19c6d812df8621
PunchCode <monetcarriere@yahoo.com>
2019-08-05 15:11:49 -0700

    final

--- form/src/App.js
+++ form/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Form from "./Form";
+import ThankYou from './ThankYou'
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div>
           <Route path="/" exact component={Form} />
+          <Route path='/thankyou' component={ThankYou}/>
         </div>
       </Router>
     );
---
+++ form/src/ThankYou.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "normalize.css/normalize.css";
+import "./Form.css";
+class Form extends React.Component {
+ 
+
+  render() {
+    return (
+      <h1>Thank You!! Your Information has been Submitted.</h1>
+    );
+  }
+}
+export default Form;
